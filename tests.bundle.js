1985620fe756b87930adf8b688337cac43979c08
Vladimir Tikhonov <tikhonov@targetprocess.com>
2018-08-31 12:54:37 +0300

    Rewrite tests using jest

--- tests.bundle.js
+++
@@ -1,3 +0,0 @@
-const context = require.context('./src', true, /\/__tests__\/(.+)\.js$/);
-
-context.keys().forEach(context);
---
+++ tests/utils/intersection.spec.js
@@ -0,0 +1,28 @@
+import {intersectRects} from 'src/utils/intersection';
+
+it('returns points of closest slice between two rects', () => {
+    const rect1 = {
+        x: 10,
+        y: 10,
+        width: 30,
+        height: 30
+    };
+
+    const rect2 = {
+        x: 50,
+        y: 10,
+        width: 30,
+        height: 30
+    };
+
+    expect(intersectRects(rect1, rect2)).toEqual({
+        start: {
+            x: 40,
+            y: 25
+        },
+        end: {
+            x: 50,
+            y: 25
+        }
+    });
+});
