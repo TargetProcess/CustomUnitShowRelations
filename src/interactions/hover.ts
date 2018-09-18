ec54a3450d10146f7695ae8cbcb04c95eff18bf5
Vladimir Tikhonov <tikhonov@targetprocess.com>
2018-09-18 17:50:13 +0300

    US#189430 Add tooltip to arrows

--- src/interactions/hover.ts
+++ src/interactions/hover.ts
@@ -1,5 +1,6 @@
 import * as $ from 'jquery';
 import Application, { IApplicationState } from 'src/application';
+import { Arrow } from 'src/arrows';
 import { isBoardConfigChanged } from 'src/utils/state';
 
 export default class Hover {
@@ -13,6 +14,7 @@ export default class Hover {
 
     private hoverArrowById(arrowId: number) {
         const arrowToHover = this.application.getState().arrows.find((arrow) => arrow.getId() === arrowId)!;
+        this.addTitleToSvg(arrowToHover);
         this.application.setState({ hoveredArrow: arrowToHover });
     }
 
@@ -22,6 +24,26 @@ export default class Hover {
 
     private getSvg() {
         return $('svg.mashupCustomUnitShowRelations__svg');
+    }
+
+    private addTitleToSvg(arrow: Arrow) {
+        const $svg = this.getSvg();
+        const title = this.findOrCreateTitleElement($svg);
+        title.textContent = arrow.isViolated() ?
+            `${arrow.getRelation().relationType}, planning issue` :
+            `${arrow.getRelation().relationType}`;
+    }
+
+    private findOrCreateTitleElement($svg: JQuery) {
+        const $existingTitle: JQuery<SVGTitleElement> = $svg.find('title') as any;
+        if ($existingTitle.length !== 0) {
+            return $existingTitle.get(0);
+        }
+
+        const title = document.createElementNS('http://www.w3.org/2000/svg', 'title');
+        title.textContent = 'SVG Title Demo example';
+        $svg.prepend(title);
+        return title;
     }
 
     private async registerHoverListenersReducer(changes: Readonly<Partial<IApplicationState>>) {
